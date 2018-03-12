99722824eada66c40c66ca8c276995bf907e2cf4
Gabriel Rimes <jivagorimes@gmail.com>
2018-03-12 20:40:50 -0300

    Add file output support (logfile)

--- index.js
+++ index.js
@@ -2,20 +2,44 @@ const Koa = require('koa')
 const Router = require('koa-router')
 const app = new Koa()
 const router = new Router()
-const { spawn } = require('child_process')
+const bodyParser = require('koa-bodyparser')
+const { spawn, execSync } = require('child_process')
+const { appendFileSync } = require('fs')
+const logfile = 'output.log'
+const port = process.env.PORT || 9200
 
-router.get('/customcmd/:cmdAlias', (ctx, next) => {
-  const ls = spawn('whoami')
+app.use(bodyParser())
 
-  ls.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`)
-  })
+execSync(`touch ${logfile}`)
 
-  console.log(JSON.stringify(ctx.query))
-  ctx.status = 200
-  ctx.body = ctx.params.cmdAlias
+router.post('/cmd', (ctx, next) => {
+  try {
+    const body = ctx.request.body
+    const childProc = spawn(body.cmd, body.args)
+
+    childProc.stdout.on('data', (data) => {
+      appendFileSync(`./${logfile}`, data, 'utf8')
+    })
+
+    childProc.stderr.on('data', (data) => {
+      appendFileSync(`./${logfile}`, data, 'utf8')
+    })
+
+    ctx.status = 200
+    ctx.body = {
+      status: true,
+      message: 'Command issued successfully.'
+    }
+  } catch (e) {
+    ctx.status = 500
+    ctx.body = {
+      status: false,
+      message: e.message
+    }
+  }
 })
 
 app.use(router.routes())
+app.listen(port)
 
-app.listen(3000)
+process.stdout.write(`Server running at port ${port}...`)
